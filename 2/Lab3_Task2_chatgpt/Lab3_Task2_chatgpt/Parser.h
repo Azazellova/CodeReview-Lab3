d697cd229b7bb365d7f7f3d81b50181835f5dbf9
Mazur Sofia <mazursofia89@yandex.ru>
2026-04-03 14:13:21 +0500

    Delete 2 directory

--- 2/Lab3_Task2_chatgpt/Lab3_Task2_chatgpt/Parser.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include <memory>
-#include <string>
-#include "tree.h"
-
-// Построение дерева из префиксного выражения
-std::shared_ptr<TreeNode> parseExpression(const std::string& expression);
